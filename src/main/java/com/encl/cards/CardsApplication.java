9eda6c8419dc9c9c5bf4fa7e3dc0bb599ea7213a
mjlslink <mjlslink5@gmail.com>
2025-09-18 11:04:20 -0500

    building master

---
+++ src/main/java/com/encl/cards/CardsApplication.java
@@ -0,0 +1,45 @@
+package com.encl.cards;
+
+import com.encl.cards.dto.CardsContactInfoDto;
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+/*@ComponentScans({ @ComponentScan("com.encl.cards.controller") })
+@EnableJpaRepositories("com.encl.cards.repository")
+@EntityScan("com.encl.cards.model")*/
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@EnableConfigurationProperties(value = {CardsContactInfoDto.class})
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Cards microservice REST API Documentation",
+				description = "EazyBank Cards microservice REST API Documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "Madan Reddy",
+						email = "tutor@encl.com",
+						url = "https://www.encl.com"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "https://www.encl.com"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "EazyBank Cards microservice REST API Documentation",
+				url = "https://www.encl.com/swagger-ui.html"
+		)
+)
+public class CardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardsApplication.class, args);
+	}
+}
